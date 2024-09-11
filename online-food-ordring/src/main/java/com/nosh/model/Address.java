d0c5fc162c2f54494d691dbd44094ceda5ac1268
Jeethan_crasta <jeethancs@gmail.com>
2024-09-11 19:11:29 +0530

    version 1

---
+++ online-food-ordring/src/main/java/com/nosh/model/Address.java
@@ -0,0 +1,18 @@
+package com.nosh.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    private Food food;
+
+
+}
---
+++ online-food-ordring/src/test/java/com/nosh/OnlineFoodOrdringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrdringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
